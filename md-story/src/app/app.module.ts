0683479a6885a4f8fd8c747471f10a77c0f1b58f
Florian Hehlen <florian.hehlen@bluewin.ch>
2018-09-25 23:51:05 +0200

    clean up and tweaks

--- md-story/src/app/app.module.ts
+++ md-story/src/app/app.module.ts
@@ -1,20 +1,17 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {Injector, NgModule} from '@angular/core';
 import {createCustomElement} from '@angular/elements';
-import {AppComponent} from './app.component';
 
 import {MkdnStoryComponent} from './mkdn-story/mkdn-story.component';
 
 @NgModule({
     declarations: [
-        AppComponent,
         MkdnStoryComponent
     ],
     imports: [
         BrowserModule
     ],
     providers: [],
-    // bootstrap: [AppComponent],
     entryComponents: [
         MkdnStoryComponent
     ]
