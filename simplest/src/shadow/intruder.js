179fb05fa8e8be380ce107a2ee418227ad3dec91
Florian Hehlen <florian.hehlen@bluewin.ch>
2018-10-04 23:48:54 +0200

    adding shadow dom example #2

---
+++ simplest/src/shadow/intruder.js
@@ -0,0 +1,16 @@
+/**
+ * Prove that the shadow DOMs are visibble or hidden
+ */
+function addIntruder(tagname) {
+
+    let root = document.getElementsByTagName(tagname)[0].shadowRoot;
+
+    if (root) {
+        let div = root.getElementById(tagname);
+        div.innerHTML = `${div.innerHTML}<p>INTRUDER CSS ADDED!</p><style>div{color:greenyellow;margin-left:33%;margin-right: 33%;}</style>`;
+        debugMessage(`[${tagname}] DOM is visible and was changed`);
+    } else {
+        debugMessage(`[${tagname}] DOM is hidden`);
+    }
+
+}
