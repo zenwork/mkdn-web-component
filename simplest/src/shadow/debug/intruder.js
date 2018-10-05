aa8522ba649384ffe291d94b0985b3e51b6f59dd
Florian Hehlen <Florian@hehlen.name>
2018-10-05 22:02:50 +0200

    example 3

--- simplest/src/shadow/debug/intruder.js
+++ simplest/src/shadow/debug/intruder.js
@@ -3,14 +3,20 @@
  */
 function addIntruder(tagname) {
 
-    let root = document.getElementsByTagName(tagname)[0].shadowRoot;
+    let element = document.getElementsByTagName(tagname)[0];
+    if (element) {
+        let root = element.shadowRoot;
 
-    if (root) {
-        let div = root.getElementById(tagname);
-        div.innerHTML = `${div.innerHTML}<p>INTRUDER CSS ADDED!</p><style>div{color:greenyellow;margin-left:33%;margin-right: 33%;}</style>`;
-        return `[${tagname}] DOM is visible and was changed`;
+        if (root) {
+            let div = root.getElementById(tagname);
+            div.innerHTML
+                = `${div.innerHTML}<p>INTRUDER CSS ADDED!</p><style>div{color:greenyellow;margin-left:33%;margin-right: 33%;}</style>`;
+            return `[${tagname}] DOM is visible and was changed.`;
+        } else {
+            return `[${tagname}] DOM is hidden.`;
+        }
     } else {
-        return `[${tagname}] DOM is hidden`;
+        return `[${tagname}] Not found.`;
     }
 
 }
