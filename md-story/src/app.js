968008361471ac2fd5329cb41e2045f245724845
Florian Hehlen <florian.hehlen@bluewin.ch>
2018-10-13 02:05:16 +0200

    refactored; removed unnecessary code

--- md-story/src/app.js
+++ md-story/src/app.js
@@ -1,21 +1,3 @@
-/* eslint-disable import/no-unassigned-import */
-
-import {LitElement, html} from '@polymer/lit-element';
 
 import './components/CounterElement.js';
 
-export default class LitApp extends LitElement {
-    static get properties() {
-        return {};
-    }
-
-    _render() {
-        return html`
-			<div>
-				<counter-element></counter-element>
-			</div>
-		`;
-    }
-}
-
-window.customElements.define('lit-app', LitApp);
