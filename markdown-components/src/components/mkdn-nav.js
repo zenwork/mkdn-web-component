3fd8fbe42a33c03bad8054fcbfa2a993b0b96e38
Florian Hehlen <flo@noumenadigital.com>
2018-11-11 15:52:11 +0100

    event linker integrated into mkdn-nav

--- markdown-components/src/components/mkdn-nav.js
+++ markdown-components/src/components/mkdn-nav.js
@@ -1,27 +1,39 @@
-import {html}         from '@polymer/lit-element/lit-element.js';
-import {repeat}       from 'lit-html/directives/repeat';
-import {ChildElement} from '../shared/child-element';
-import {
-  dispatchHashUrl,
-  initFromAndObserveContent, listenForIndexUpdate,
-  listenForSelection,
-}                     from '../shared/events';
-import styles         from './mkdn-nav.css.js';
+import {html}            from '@polymer/lit-element/lit-element.js';
+import {repeat}          from 'lit-html/directives/repeat';
+import {ChildElement}    from '../shared/child-element';
+import {dispatchHashUrl} from '../shared/events';
+import EventLinker       from '../shared/eventLinker';
+import styles            from './mkdn-nav.css.js';
 
+/**
+ * MkDn Navigation component
+ */
 export class MkdnNav extends ChildElement {
-  static get name() {return 'mkdn-nav';}
 
-  constructor() {
-    super();
-    this.delimiter = '>';
+  static get name() {
+    return 'mkdn-nav';
   }
 
+  /**
+   * watched properties
+   * @return {Object}
+   */
   static get properties() {
     return {
       crumbs: {type: Array, attribute: false},
       delimiter: {type: String},
       view: {type: String},
+      state: {type: Object, attribute: false},
     };
+  }
+
+  /**
+   * constructor
+   */
+  constructor() {
+    super();
+    this.delimiter = '>';
+    this.register = new EventLinker(this, true);
   }
 
   connectedCallback() {
@@ -29,95 +41,119 @@ export class MkdnNav extends ChildElement {
   }
 
   initStandalone() {
-    this.observer = initFromAndObserveContent('MKDN-NAV',
-                                              function(input, root) {
-                                                root.crumbs = JSON.parse(input.trim());
-                                                MkdnNav.updatePageLink(root);
-                                              },
-                                              this);
+    this.register.listener('observable', ['this'], this.onElementContentChange);
+    this.register.startListening(this);
+    this.onElementContentChange(this.innerHTML);
   }
 
   onAccepted(parent) {
+    this.register.listener('hashchange', ['window'], this.onHashChange);
+    this.register.listener('mkdn-list-selection', ['mkdn-list'], this.onSelection);
+    this.register.listener('mkdn-store-index-updated', ['mkdn-store', 'mkdn-static-store'], this.onIndexChanged);
 
-    window.addEventListener('hashchange', () => {
-      let hash = window.location.hash.substr(1);
+    this.register.dispatcher('this', 'mkdn-nav-hash-url');
 
-      if (hashHasChanged(hash, this.crumbs) || hasNoCrumbs(hash, this.crumbs)) {
-        // console.log(`dispatch: ${hash}`);
-        dispatchHashUrl(this, hash);
-        MkdnNav.updatePageLink(this);
-      }
-
-      function hasNoCrumbs(hash, crumbs) {
-        return (hash && !crumbs);
-      }
-
-      function hashHasChanged(hash, crumbs) {
-        return (hash && crumbs && hash !== crumbs[crumbs.length - 1]);
-      }
-
-    });
+    this.register.startListening(window);
 
     this.ready();
   }
 
-  static updatePageLink(root) {
-    let crumb = root.crumbs[root.crumbs.length - 1];
-    history.pushState({}, crumb.name, crumb.link);
-    document.title = crumb.name;
-  }
 
   onSiblingReady(sibling) {
     switch (sibling.Class) {
       case 'mkdn-list':
-        listenForSelection(sibling, (event) => {
-          const storyDef = event.detail;
-          if(!this.crumbs) this.crumbs=[];
-          if (this.crumbs && this.crumbs.length > 1) {
-            this.crumbs.pop();
-          }
-          this.crumbs.push(
-            {id: storyDef.url, name: storyDef.title, link: `#${storyDef.url}`});
-          this.crumbs = [...this.crumbs];
-          MkdnNav.updatePageLink(this);
-        });
-
+        this.register.startListening(sibling);
         break;
       case 'mkdn-store':
-      case 'md-tatic-store':
-        listenForIndexUpdate(sibling, (event) => {
-          let index = event.detail;
-          let parent = index.section;
-          let crumbs = [];
-          crumbs.push({id: parent.url, name: parent.title, link: parent.url});
-          this.crumbs = crumbs;
-        });
-
+      case 'md-static-store':
+        this.register.startListening(sibling);
         if (window.location.hash) {
-          dispatchHashUrl(this, window.location.hash.substr(1));
+          this.register.dispatch('mkdn-nav-hash-url', window.location.hash.substr(1));
         }
         break;
     }
   }
 
+  /**
+   * render element
+   * @return {TemplateResult}
+   */
   render() {
     return html`
-			${styles(this.delimiter)} 
-			${this.renderCrumbs()}`;
+      ${styles(this.delimiter)} 
+      ${this.renderCrumbs()}`;
   }
 
+  /**
+   * @return {TemplateResult}
+   */
   renderCrumbs() {
     if (this.crumbs && this.crumbs.length > 0) {
       return html`
           <ul class="mkdn-list">
-					${repeat(this.crumbs,
-                   (crumb) => { return crumb.id;},
-                   (crumb) => {return html`<li><a href="${crumb.link}">${crumb.name}</a></li>`;})}
-					</ul>`;
+          ${repeat(this.crumbs,
+                   (crumb) => {
+                     return crumb.id;
+                   },
+                   (crumb) => {
+                     return html`<li><a href="${crumb.link}">${crumb.name}</a></li>`;
+                   })}
+          </ul>`;
     } else {
       return html`<ul class="mkdn-list"><li>&nbsp;</li></ul>`;
     }
   }
+
+  static updatePageLink(root) {
+    const crumb = root.crumbs[root.crumbs.length - 1];
+    history.pushState({}, crumb.name, crumb.link);
+    document.title = crumb.name;
+  }
+
+  onElementContentChange(input) {
+    if (input) {
+      this.crumbs = JSON.parse(input.trim());
+      MkdnNav.updatePageLink(this);
+    }
+  }
+
+  onHashChange() {
+    const hash = window.location.hash.substr(1);
+
+    if (hashHasChanged(hash, this.crumbs) || hasNoCrumbs(hash, this.crumbs)) {
+      // console.log(`dispatch: ${hash}`);
+      dispatchHashUrl(this, hash);
+      MkdnNav.updatePageLink(this);
+    }
+
+    function hasNoCrumbs(hash, crumbs) {
+      return (hash && !crumbs);
+    }
+
+    function hashHasChanged(hash, crumbs) {
+      return (hash && crumbs && hash !== crumbs[crumbs.length - 1]);
+    }
+  };
+
+  onSelection(event) {
+    const storyDef = event.detail;
+    if (!this.crumbs) this.crumbs = [];
+    if (this.crumbs && this.crumbs.length > 1) {
+      this.crumbs.pop();
+    }
+    this.crumbs.push({id: storyDef.url, name: storyDef.title, link: `#${storyDef.url}`});
+    this.crumbs = [...this.crumbs];
+    MkdnNav.updatePageLink(this);
+  };
+
+  onIndexChanged(event) {
+    const index = event.detail;
+    const parent = index.section;
+    const crumbs = [];
+    crumbs.push({id: parent.url, name: parent.title, link: parent.url});
+    this.crumbs = crumbs;
+  };
+
 }
 
 MkdnNav.define();
---
+++ markdown-components/src/shared/eventLinker.js
@@ -0,0 +1,138 @@
+/**
+ * Sate management convenience
+ */
+export default class EventLinker {
+
+  /**
+   * Construct default state
+   * @param {Element} root requiring event linking
+   * @param {Boolean} debug optional, proivide true to enable;
+   * otherwise defaults to false.
+   */
+  constructor(root, debug) {
+    this.root = root;
+    this.listenables = [];
+    this.dispatched = [];
+    this.debug = debug;
+  }
+
+  /**
+   * Register events interested in
+   * @param {String} event
+   * @param {Array} forElements
+   * @param {function} doAction that will be bind() to the context provided
+   * at construction
+   */
+  listener(event, forElements, doAction) {
+    this.listenables.push({eventName: event, elementNames: forElements, fn: doAction});
+  }
+
+  /**
+   * start Listening to events interested in dispatched by a listenable
+   * @param {Element} listenable
+   */
+  startListening(listenable) {
+    let listenableName;
+    if (listenable.self === window) {
+      listenableName = 'window';
+    } else if (listenable === this.root) {
+      listenableName = 'this';
+    } else {
+      listenableName = listenable.tagName;
+    }
+
+    this.listenables.map((entry) => {
+      entry.elementNames.map((name) => {
+        if (name.toUpperCase() === listenableName.toUpperCase()) {
+          const listenerFn = entry.fn;
+          const boundFn = listenerFn.bind(this.root);
+          if (entry.eventName === 'observable') {
+            this.observeContentChange(boundFn, listenable);
+          } else {
+            listenable.addEventListener(entry.eventName, this.wrap(boundFn));
+          }
+        }
+      });
+    });
+  }
+
+  wrap(fn) {
+    if (!this.debug) {
+      return fn;
+    } else {
+      return function(...args) {
+        console.log(`calling ${fn.name}`);
+        return fn(...args);
+      };
+    }
+  }
+
+  /**
+   * register a dispatching config
+   * @param {String} fromElement
+   * @param {String} event
+   */
+  dispatcher(fromElement, event) {
+    this.dispatched.push({source: fromElement, eventName: event});
+  }
+
+  /**
+   * Dispatch for registered events
+   * @param {String} eventName
+   * @param {Object} data
+   */
+  dispatch(eventName, data) {
+    this.dispatched.map((dispatchable) => {
+      if (dispatchable.eventName === eventName) {
+        let source;
+        if (dispatchable.source === 'this') {
+          source = this.root;
+        } else {
+          source = dispatchable.source;
+        }
+        if (data) {
+          const detail = {detail: data};
+          const event = new CustomEvent(dispatchable.eventName, detail);
+          source.dispatchEvent(event);
+        } else {
+          const event = new CustomEvent(dispatchable.eventName);
+          source.dispatchEvent(event);
+        }
+      }
+    });
+  }
+
+  /**
+   * util function that creates and triggers the observation of the content of a
+   * given node.
+   *
+   * @param {function} handleInputFn function to handle the mutation
+   * @param {Element} observable element to observe
+   * @return {MutationObserver} observer
+   */
+  observeContentChange(handleInputFn, observable) {
+    // Options for the observer (which mutations to observe)
+    const config = {attributes: false, childList: true, subtree: false};
+    const observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        if (mutation.type === 'childList'
+            && mutation.target.nodeName === observable.tagName.toUpperCase()) {
+          const input = mutation.target.innerHTML;
+          try {
+            if (input) {
+              handleInputFn(input, observable);
+            }
+          } catch (e) {
+            throw e;
+          }
+        }
+      });
+    });
+
+    // Start observing the target node for configured mutations
+    observer.observe(observable, config);
+    return observer;
+  }
+
+}
+
